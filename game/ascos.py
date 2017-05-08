7c7694a0b909ea89b48fadca9113161bbdb81184
Jack Nelson <jwnelsoniv@gmail.com>
2017-05-07 18:59:51 -0700

    Worked on the spacecraft OS. First pass at generating a list of available programs based on the files available in the programs/ directory.

--- game/ascos.py
+++ game/ascos.py
@@ -1,22 +1,54 @@
+import sys, os
+import importlib
+
 class ASCOS:
 	"""
-		Autonomous SpaceCraft Operating System
+		Autonomous Spacecraft Operating System
 
-		The main operating system for the spacecraft that the player interfaces with
+		The main operating system for the spacecraft that the player interfaces with.
+		Parses user input line by line for commands and runs the specified programs.
 	"""
-	def __init__(self, programs):
-		self.programs = programs # dict of available programs
+
+	def __init__(self, player, spacecraft, gameinfo):
+		self.player = player
+		self.spacecraft = spacecraft
+		self.gameinfo = gameinfo
+
+		self.programs = self._generate_program_dict() # get a dict of all available programs
+
+	def _generate_program_dist(self):
+		"""
+			Automatically generate a list of programs from modules in programs/
+		"""
+		programs = {}
+		for program in os.listdir("programs"):
+			if program != "program.py" and program != "__init__.py":
+				programs.append(program.rstrip(".py"))
+
+		return programs
 
 
-	def cd(self, newdir):
-		pass
 
-	def ls(self):
-		pass
+	def handle_input(self, rawinput):
+		"""
+			Handles player input to the terminal.
+		"""
+		input_ = rawinput.split('')
+		program = None
+		options = None
 
-	def help(self):
-		pass
+		# parse the command and options from the input
+		if input_[0] in self.commands:
+			program = self.programs[input_]
+			if len(input_) > 1:
+				options = input_[1:]
+			else:
+				options = []
 
-	def run_program(self, program):
-		pass
+			# run the program with the given options list 
+			result = program.run(options)
+		else:
+
+
+			
 
